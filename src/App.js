beda7c106ca41c29341aef153443c67a9ec38c81
Benoît Hubert <benoithubert@gmail.com>
2018-06-22 10:33:09 +0200

    Passage de props au composant

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import { Switch, Route, BrowserRouter as Router } from 'react-router-dom'
 import Layout from './Layout'
 
 const Home = () => <h1>Home</h1>
-const About = () => <h1>About</h1>
+const About = props => (
+  <div>
+    <h1>About</h1>
+    <p>{props.text}</p>
+  </div>
+)
 const User = () => <h1>User</h1>
 const NoMatch = () => <h1>404 Not Found</h1>
 
@@ -13,8 +18,8 @@ class App extends Component {
       <Router>
         <Switch>
           <Layout exact path="/" component={Home}/>
-          <Layout path="/about" component={About}/>
-          <Layout path="/user/:user" component={User}/>
+          <Layout path="/about" component={About} text="This is the About page" />
+          <Layout path="/user/:user" component={User} />
           <Layout component={NoMatch}/>
         </Switch>
       </Router>
--- src/Layout.js
+++ src/Layout.js
@@ -2,11 +2,16 @@ import React, { Fragment } from 'react'
 import { Route } from 'react-router-dom'
 import Navbar from './Navbar'
 
-const Layout = props => (
-  <Fragment>
-    <Navbar />
-    <Route {...props} />
-  </Fragment>
-)
+// Higher Order Component  ou  HOC
+// https://reactjs.org/docs/higher-order-components.html
+const Layout = ({ exact, path, component: Component, ...rest }) => {
+  console.log(rest)
+  return (
+    <Fragment>
+      <Navbar />
+      <Route exact={exact} path={path} render={() => <Component {...rest} />} />
+    </Fragment>
+  )
+}
 
 export default Layout
